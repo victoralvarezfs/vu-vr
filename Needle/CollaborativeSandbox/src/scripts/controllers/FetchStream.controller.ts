5ee6614be95d0cc747eb534769c215f9c3bb27ba
Bruno Dias <brunodg.dev@gmail.com>
2023-03-06 18:58:25 -0300

    feat: Including event data, users and streams.

---
+++ Needle/CollaborativeSandbox/src/scripts/controllers/FetchStream.controller.ts
@@ -0,0 +1,47 @@
+import { Request, Response, NextFunction } from "express";
+import axios, { AxiosResponse } from "axios";
+import {
+  EventStream,
+  User,
+  Stream,
+  Subscriber,
+  Subscription,
+} from "../interfaces/StreamResponse.interface";
+
+const baseUrl = "";
+
+/**
+ * Fetching all streams available
+ *
+ * @param req request
+ * @param res response
+ * @param next
+ * @returns links of all streams online
+ */
+const GetAllStreams = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  let result: AxiosResponse = await axios.get(`${baseUrl}/api/streams`);
+  let event: EventStream = result.data;
+  let user: User = event.User;
+  let streams: Stream[] = user.Streams;
+  return res.status(200).json({
+    streams: streams,
+  });
+};
+
+const GetStreamFromUser = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  let result: AxiosResponse = await axios.get(`${baseUrl}/api/streams/search/`);
+  let event: EventStream = result.data;
+  let user: User = event.User;
+  let streams: Stream[] = user.Streams;
+  return res.status(200).json({
+    streams: streams,
+  });
+};
---
+++ Needle/CollaborativeSandbox/src/scripts/interfaces/StreamResponse.interface.ts
@@ -0,0 +1,94 @@
+export interface EventStream {
+  image: String;
+  id: String;
+  title: String;
+  price: Number;
+  date?: Date;
+  streamKey: String;
+  playbackId: String;
+  status: String;
+  createdAt: Date;
+  updatedAt: Date;
+  UserId: String;
+  CategoryId: Number;
+  User: User;
+  Ratings?: Rating[];
+  Tags?: Tag[];
+  avgRating?: Number;
+}
+
+export interface User {
+  avatar: String;
+  coverPhoto?: String;
+  id: String;
+  firstName: String;
+  lastName: String;
+  username?: String;
+  displayName?: String;
+  email: String;
+  phone: String;
+  password: String;
+  passcode?: String;
+  passcodeExpiry?: Date;
+  passwordExpiry?: Date;
+  passwordResetToken?: String;
+  deviceId?: String;
+  notificationStatus: Boolean;
+  aboutDescription?: String;
+  links?: String;
+  avgRating?: String;
+  stripeCreatorAccountId?: String;
+  stripeCustomerId?: String;
+  stripeCustomerBankToken?: String;
+  createdAt: Date;
+  updatedAt: Date;
+  roleId: Number;
+  Subscribers?: Subscriber[];
+  Subscriptions?: Subscription[];
+  Streams: Stream[];
+}
+
+export interface Subscriber {
+  id: String;
+  firstName: String;
+  lastName: String;
+}
+
+export interface Subscription {
+  id: String;
+  firstName: String;
+  lastName: String;
+}
+
+export interface Stream {
+  image: String;
+  title: String;
+  id: String;
+  price: Number;
+  playbackId: String;
+  status: String;
+  date?: Date;
+  streamKey: String;
+  UserId: String;
+}
+
+export interface RatingCheck {
+  firstName: String;
+  lastName: String;
+  rating: Rating;
+}
+
+export interface Rating {
+  rating: Number;
+  createdAt: Date;
+  updatedAt: Date;
+  UserId: String;
+  StreamId: String;
+}
+
+export interface Tag {
+  id: Number;
+  name: String;
+  createdAt: Date;
+  updatedAt: Date;
+}
