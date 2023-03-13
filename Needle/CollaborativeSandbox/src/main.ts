65e7a6e2087f2e3e40381496bf912e19361a08a4
Bruno Dias <brunodg.dev@gmail.com>
2023-03-13 20:01:20 -0300

    feat: Lobby room UI first screen created

--- Needle/CollaborativeSandbox/src/main.ts
+++ Needle/CollaborativeSandbox/src/main.ts
@@ -1,1 +1,9 @@
 import "@needle-tools/engine";
+
+export function moveMicButton() {
+  const micButton = document.getElementById("");
+}
+
+function changeMenu(x) {
+  x.classList.toggle("change");
+}
